90c683670bb534841337e3e6a06c2b792a979b38
cameo choquer <cjchoquer@gmail.com>
2023-03-01 20:24:40 +0000

    adding files & user stories to js file

---
+++ script.js
@@ -0,0 +1,20 @@
+// Add tasks to a list so that I can keep track of them
+
+test("Submitting a new task adds it to the list", () => {
+    // test goes here
+  });
+
+//   Check things off my list so that I can see what I’ve done
+test("Checking an entry marks it as complete", () => {
+    // test goes here
+  });
+
+//   Delete things from the list if I don’t need to do them anymore
+test("Deleting an entry removes it from the list", () => {
+    // test goes here
+  });
+
+//   Filter out completed to-dos from my list so that I can focus on what’s left to do
+test("Toggling the filter hides completed tasks from the list", () => {
+    // test goes here
+  });
