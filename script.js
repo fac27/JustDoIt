d5a5fc1955ab0938defca68f187e71d5eaae645f
Malcolm Wilson <malcolmwilsonmcg@gmail.com>
2023-03-04 15:09:38 +0000

    Added scripts for deleteItem and filterComplete

--- script.js
+++ script.js
@@ -14,11 +14,27 @@ test("Checking an entry marks it as complete", () => {
 });
 
 //   Delete things from the list if I don’t need to do them anymore
-test("Deleting an entry removes it from the list", () => {
-  // test goes here
-});
+//   Link to a button with click event listener
+element.addeventlistner('click', deleteItem());
+
+const deleteItem = () => {
+
+  const itemList = [];
+
+  return index => {
+    itemList.splice(index, 1);
+    return itemList;
+  }
+};
 
 //   Filter out completed to-dos from my list so that I can focus on what’s left to do
-test("Toggling the filter hides completed tasks from the list", () => {
-  // test goes here
-});
+//   Link to a button with click event listener
+element.addeventlistner('click', filterComplete());
+
+const filterComplete = () => {
+
+  const taskList = [];
+  let filterCompleted = false;
+
+  return taskList.filter(task => !filterCompleted || !task.completed);
+};
